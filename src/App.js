204524264dc07acbf97e7d24eaa948ca9594d7ef
Marlon Aragon <aragonmarlon1@gmail.com>
2023-05-03 15:50:32 +0800

    :rocket: deploy navbar :fire: change cdn for bootstrap :white_check_mark: added cdn for fontawesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./pages/Home";
 import Features from "./pages/Features";
 import SignUp from "./pages/SignUp";
@@ -7,21 +7,13 @@ import NotFound from "./pages/NotFound";
 import Pricing from "./pages/Pricing";
 import WelcomePage from "./pages/WelcomePage";
 import Plans from "./pages/Plans";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/features">Features</Link></li>
-            <li><Link to="/signup">Sign up</Link></li>
-            <li><Link to="/pricing">Pricing</Link></li>
-            <li><Link to="/welcome-page">Welcome Page</Link></li>
-            <li><Link to="/plans">Plans</Link></li>
-          </ul>
-        </nav>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/features" element={<Features />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand fw-bold" href="#/"><span className="fw-bold fs-2 text-primary">B</span>rollo</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center gap-2">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-item text-decoration-none text-secondary">Home</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to="/features" className="nav-item text-decoration-none text-secondary">Features</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to="/pricing" className="nav-item text-decoration-none text-secondary">Pricing</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to="/plans" className="nav-item text-decoration-none text-secondary">Plans</Link>
+                            </li>
+
+                        </ul>
+                        <div className="d-flex gap-1">
+                            <Link to="/signup" className="nav-item text-decoration-none text-white bg-primary rounded p-2">SignUp</Link>
+                            <Link to="login" className="nav-item text-decoration-none text-white bg-primary rounded p-2">Login</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
