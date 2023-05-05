9de7c860f03bc5d0fa2dae5affef28567abf37a2
Marlon Aragon <aragonmarlon1@gmail.com>
2023-05-05 15:01:37 +0800

    :white_check_mark: added navigation to facebook sign in button

--- src/components/login-components/FacebookLogin.js
+++ src/components/login-components/FacebookLogin.js
@@ -1,23 +1,34 @@
 import { LoginSocialFacebook } from 'reactjs-social-login';
 import { FacebookLoginButton } from 'react-social-login-buttons';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Login.css";
 
 function FacebookLogin() {
     const [profile, setProfile] = useState(null);
+    const navigate = useNavigate();
+
+    const handleResolve = (response, data) => {
+        console.log(response);
+        navigate('/Features');
+    }
+
+    const handleReject = (err) => {
+        console.log(err);
+    }
+
+    const showProfile = (data) => [
+        setProfile(data)
+    ]
 
     return (
         <>
             <div className='brollo-fb'>
                 {!profile ? <LoginSocialFacebook
                     appId="630216948536391"
-                    onResolve={(response) => {
-                        console.log(response);
-                        setProfile(response.data);
-                    }}
-                    onReject={(error) => {
-                        console.log(error);
-                    }}
+                    onResolve={handleResolve}
+                    onReject={handleReject}
+                    onSubmit={showProfile}
                 >
                     <FacebookLoginButton />
                 </LoginSocialFacebook> : ""}
