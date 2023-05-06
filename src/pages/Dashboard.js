3490fc9341ac29609a87f072f81b949561098c27
ChrisVerdida <christian.verdida123@gmail.com>
2023-05-06 18:06:41 +0800

    Adding Style DashBoard For The Welcome Title

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Sidebar from '../components/dashboard-components/Sidebar/Sidebar';
 import Projects from '../components/dashboard-components/Projects/Projects';
 import Template from '../components/dashboard-components/Templates/Templates';
+import "./pageStyles/Dashboard.css";
 function Dashboard() {
     return (
         <>
+          <p className="UserName">Hi, UserName</p>
+          <p className="Welcome">Welcome to your dashboard.</p>
           <Sidebar />
           <Template />
           <Projects />  
