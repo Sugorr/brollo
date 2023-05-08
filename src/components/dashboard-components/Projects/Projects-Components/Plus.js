05c2ca508a946598b821f3cedc37342ba5045619
ChrisVerdida <christian.verdida123@gmail.com>
2023-05-08 15:54:16 +0800

    Adding Solo Gear for Plus Funtionality

--- src/components/dashboard-components/Projects/Projects-Components/Plus.js
+++ src/components/dashboard-components/Projects/Projects-Components/Plus.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./Plus.css";
+import SoloGear from "./SoloGear";
 function Plus() {
+    const [plus, setPlus] = useState(['Plus']);
+    const addGear = () => {
+        setPlus([...plus, `Plus ${plus.length + 1}`]);
+    };
+    const removeGear = () => {
+        if (plus.length > 0) {
+            setPlus(plus.slice(0, -1));
+        }
+    };
     return(
-        <>
-            <div className="Plus-Container">
-                <div className="btn add-project-btn">
-                    <i class="fa-solid fa-plus"></i>
+        <>  
+            <div className="SoloGear-Container">
+                {plus.map(plus => (
+                    <SoloGear key={plus}/>
+                ))}
+                <div className="Plus-Container"> 
+                    <div className="btn add-project-btn">
+                        <i onClick={addGear} className="fa-solid fa-plus"></i>
+                        <p className="New-Project">New Project</p>
+                    </div>
                 </div>
             </div>
         </>
---
+++ src/components/dashboard-components/Projects/Projects-Components/SoloGear.js
@@ -0,0 +1,12 @@
+import "./SoloGear.css";
+function SoloGear() {
+    return(
+        <>
+            <div className="Project-SoloGear">
+                <i className="fa-solid fa-gear SoloGear-Icon"></i>
+                <p className="Database-Project-SoloGear">Database Project</p>
+            </div>
+        </>
+    )
+}
+export default SoloGear;
