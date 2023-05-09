5aee5849e069178078a5565f643101fafe6040e2
Joaquin Reynera <127176242+Sugorr@users.noreply.github.com>
2023-05-09 18:10:10 +0800

    completed carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,30 +3,55 @@ import React, { useState } from 'react';
 
 // carousel-points btn active-point
 export default function Carousel(){
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    // Define an array of carousel slide content
+    const carouselSlides = [
+      {
+        id: 1,
+        text: 'Brollo is great for simplifying complex processes. As a manager, I can chunk processes down into bite-sized pieces for my team and then delegate that out, but still keep a bird’s-eye view.',
+        statistic: '75% of organizations report that Brollo delivers value to their business within 30 days.'
+      },
+      {
+        id: 2,
+        text: 'Another slide content goes here',
+        statistic: 'Another statistic goes here'
+      },
+      {
+        id: 3,
+        text: 'Yet another slide content goes here',
+        statistic: 'Yet another statistic goes here'
+      }
+    ];
+  
+    // Define a function to handle clicking on the angle buttons
+    const handleAngleClick = (direction) => {
+      if (direction === 'left') {
+        setActiveIndex((activeIndex - 1 + carouselSlides.length) % carouselSlides.length);
+      } else {
+        setActiveIndex((activeIndex + 1) % carouselSlides.length);
+      }
+    };
+
+    function skipCarousel(index){
+        setActiveIndex((index) % carouselSlides.length);
+    }
+  
     return (
-        <>
-            <div className='container-fluid p-5 d-flex flex-column'>
-                <div className='carousel-container'>
-                    <p>
-                        [Brollo is] great  for simplyfing complex processes.
-                        As a manager,
-                        I can chunk [processes] down into bite-sized pieces for my team and then delegate that out,
-                        but still keep a bird’s-eye view. 
-                    </p>
-                    <h1>
-                        75% of orgranizations report that Brollo delivers value to their business within 30days.
-                    </h1>
-                    <div className='carousel-points-container'>
-                        <button type='checkbox' className="carousel-points btn active-point"></button>
-                        <button type='checkbox' className="carousel-points btn"></button>
-                        <button type='checkbox' className="carousel-points btn"></button>
-                        <div className='d-flex angle-btn'>
-                            <button><i class="fa-solid btn fa-angle-left"></i></button>
-                            <button><i class="fa-solid btn fa-angle-right"></i></button>
-                        </div>
-                    </div>
-                </div>
+      <div className="container-fluid p-5 d-flex flex-column">
+        <div className="carousel-container">
+          <p>{carouselSlides[activeIndex].text}</p>
+          <h1>{carouselSlides[activeIndex].statistic}</h1>
+          <div className="carousel-points-container">
+            {carouselSlides.map((slide, index) => (
+              <div key={slide.id} onClick={() => skipCarousel(index)} className={`carousel-points btn ${index === activeIndex ? 'active-point' : ''}`}></div>
+            ))}
+            <div className="d-flex angle-btn">
+              <button onClick={() => handleAngleClick('left')}><i className="fa-solid btn fa-angle-left"></i></button>
+              <button onClick={() => handleAngleClick('right')}><i className="fa-solid btn fa-angle-right"></i></button>
             </div>
-        </>
+          </div>
+        </div>
+      </div>
     );
 }
--- src/components/PowerHouse.js
+++ src/components/PowerHouse.js
@@ -1,28 +1,50 @@
+import React, { useState } from 'react';
 
+export default function PowerHouse() {
+  const [activeBoardIndex, setActiveBoardIndex] = useState(0);
 
-export default function PowerHouse(){
-    return(
-        <>
-            <div className='container-fluid p-5 powerhouse' style={{color: '#1B2134'}}>
-                <h5 className='mx-5 px-5 py-2'>Brollo</h5>
-                <h1 className='mx-5 px-5 py-2'>A productivity powerhouse</h1>
-                <h3 className='mx-5 px-5 py-2 w-75'>
-                    Simple, flexible, and powerful.
-                    All it takes are boards, lists,
-                    and cards to get a clear view of who’s doing what and what needs to get done.
-                    Learn more in our guide for getting started.
-                </h3>
-                <div className='container-fluid px-5 m-5 d-flex'>
-                    <div className='board-section'>
-                        <div className='board board-selected'>Board 1</div>
-                        <div className='board'>Board 2</div>
-                        <div className='board'>Board 3</div>
-                    </div>
-                    <div className='slide-section w-75'>
-                        <div className='test'></div>
-                    </div>
-                </div>
+  // Define an array of boards
+  const boards = [
+    'Brollo boards keep tasks organized and work moving forward. In a glance everything from “things to do” to “ aww yeah, we did it!”', 
+    'Brollo boards keep tasks organized and work moving forward. In a glance everything from “things to do” to “ aww yeah, we did it!”', 
+    'Brollo boards keep tasks organized and work moving forward. In a glance everything from “things to do” to “ aww yeah, we did it!”'
+    ];
+
+  // Define an array of carousel slide content
+  const carouselSlides = [    {      id: 1,      text: 'Slide 1 content goes here',      statistic: 'Slide 1 statistic goes here'    },    {      id: 2,      text: 'Slide 2 content goes here',      statistic: 'Slide 2 statistic goes here'    },    {      id: 3,      text: 'Slide 3 content goes here',      statistic: 'Slide 3 statistic goes here'    }  ];
+
+  // Define a function to handle clicking on the board buttons
+  const handleBoardClick = (index) => {
+    setActiveBoardIndex(index);
+  };
+
+  return (
+    <>
+      <div className="container-fluid p-5 powerhouse" style={{ color: '#1B2134' }}>
+        <h5 className="mx-5 px-5 py-2">Brollo</h5>
+        <h1 className="mx-5 px-5 py-2">A productivity powerhouse</h1>
+        <h3 className="mx-5 px-5 py-2 w-75">
+          Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who’s doing what and what needs to get done. Learn more in our guide for getting started.
+        </h3>
+        <div className="container-fluid px-5 m-5 d-flex">
+          <div className="board-section">
+            {/* Map through the board array and render a button for each board */}
+            {boards.map((board, index) => (
+                <button key={index} onClick={() => handleBoardClick(index)} className={`board ${index === activeBoardIndex ? 'board-selected' : ''}`}>
+                    <h5>Boards</h5>
+                    {board}
+                </button>
+            ))}
+          </div>
+          <div className="slide-section w-75">
+            {/* Display the slide content corresponding to the active board index */}
+            <div className="test">
+              <p>{carouselSlides[activeBoardIndex].text}</p>
+              <h1>{carouselSlides[activeBoardIndex].statistic}</h1>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
