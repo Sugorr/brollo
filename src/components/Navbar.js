7f2f2c27d4aafc80e7f4ffd5c9de097b15d75b40
ChrisVerdida <christian.verdida123@gmail.com>
2023-05-11 04:26:18 +0800

    Adding Enhance Changes For NavBar in Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,47 @@
 import { Link } from "react-router-dom";
-
+import "./Navbar.css"
 function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary py-0">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <h3 className="navbar-brand fw-bold">Brollo</h3>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center gap-2">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-item text-decoration-none text-secondary">Home</Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/features" className="nav-item text-decoration-none text-secondary">Features</Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/pricing" className="nav-item text-decoration-none text-secondary">Pricing</Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/plans" className="nav-item text-decoration-none text-secondary">Plans</Link>
-                            </li>
-
-                        </ul>
-                        <div className="d-flex gap-1">
-                            <Link to="/signup" className="nav-item text-decoration-none text-white bg-primary rounded p-2">SignUp</Link>
-                            <Link to="login" className="nav-item text-decoration-none text-white bg-primary rounded p-2">Login</Link>
-                        </div>
-                    </div>
+                    <h3 className="Home-Brollo fw-bold">
+                        <div className="Blue-b">B</div>
+                        <div className="Rollo">rollo</div>
+                    </h3>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center gap-2">
+                        <li className="nav-item">
+                            <Link to="/" className="Nav-Home nav-item text-decoration-none 
+                            text-secondary">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/features" className="Nav-Features nav-item text-decoration-none 
+                            text-secondary">Features</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/pricing" className="Nav-Pricing nav-item text-decoration-none 
+                            text-secondary">Pricing</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/plans" className="Nav-Plans nav-item text-decoration-none 
+                            text-secondary">Plans</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/plans" className="Nav-Resources nav-item text-decoration-none 
+                            text-secondary">Resources</Link>
+                        </li>
+                        <li className="nav-item">    
+                            <Link to="login" className="Nav-Login text-decoration-none">
+                            <i class="fa-solid fa-user"></i>Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="Nav-Get-Brollo text-decoration-none">
+                            Get Brollo</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
     );
 }
-
 export default Navbar;
