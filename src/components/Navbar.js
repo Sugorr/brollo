b6aca90d34f3b05b919a628160f9fbfe9927e743
ChrisVerdida <christian.verdida123@gmail.com>
2023-05-11 06:00:39 +0800

    Adding Arrange Clean Code and Home Responsive Fixing Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,45 +2,47 @@ import { Link } from "react-router-dom";
 import "./Navbar.css"
 function Navbar() {
     return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <h3 className="Home-Brollo fw-bold">
-                        <div className="Blue-b">B</div>
-                        <div className="Rollo">rollo</div>
-                    </h3>
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center gap-2">
-                        <li className="nav-item">
-                            <Link to="/" className="Nav-Home nav-item text-decoration-none 
-                            text-secondary">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/features" className="Nav-Features nav-item text-decoration-none 
-                            text-secondary">Features</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/pricing" className="Nav-Pricing nav-item text-decoration-none 
-                            text-secondary">Pricing</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/plans" className="Nav-Plans nav-item text-decoration-none 
-                            text-secondary">Plans</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/plans" className="Nav-Resources nav-item text-decoration-none 
-                            text-secondary">Resources</Link>
-                        </li>
-                        <li className="nav-item">    
-                            <Link to="login" className="Nav-Login text-decoration-none">
-                            <i class="fa-solid fa-user"></i>Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="Nav-Get-Brollo text-decoration-none">
-                            Get Brollo</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <>  
+            <div id="Home-Navbar-Section">
+                <nav className="Home-Navbar navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <h3 className="Home-Brollo fw-bold">
+                            <div className="Blue-Home-b">B</div>
+                            <div className="Rollo-Home">rollo</div>
+                        </h3>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center gap-2">
+                            <li className="nav-item">
+                                <Link to="/" className="Nav-Home nav-item text-decoration-none 
+                                text-secondary">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/features" className="Nav-Features nav-item text-decoration-none 
+                                text-secondary">Features</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/pricing" className="Nav-Pricing nav-item text-decoration-none 
+                                text-secondary">Pricing</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/plans" className="Nav-Plans nav-item text-decoration-none 
+                                text-secondary">Plans</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/plans" className="Nav-Resources nav-item text-decoration-none 
+                                text-secondary">Resources</Link>
+                            </li>
+                            <li className="nav-item">    
+                                <Link to="login" className="Nav-Login text-decoration-none">
+                                <i class="fa-solid fa-user"></i>Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="Nav-Get-Brollo text-decoration-none">
+                                Get Brollo</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </>
     );
 }
--- src/components/dashboard-components/Sidebar/Sidebar.js
+++ src/components/dashboard-components/Sidebar/Sidebar.js
@@ -2,32 +2,35 @@ import "./Sidebar.css";
 function Sidebar() {
     return(
         <>  
-            <div className="sidebar-btn" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
-                <i className="fa-solid fa-bars"></i>
-            </div>  
-            <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" 
-                tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
-                       <div className="Blue-B">B</div>
-                       <div className="Rollo">rollo</div>
-                    </h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <div className="Side-Chest">Home</div>
-                    <div  className="Side-Chest">Boards</div>
-                    <div className="dropend">
-                        <div className="Side-Chest-Toggle dropdown-toggle" 
-                            data-bs-toggle="dropdown" aria-expanded="false" type="button">
-                            Dropdown
+            <div id="Sidebar-Section">
+                <div className="sidebar-btn" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+                    <i className="fa-solid fa-bars"></i>
+                </div>  
+                <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" 
+                    tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
+                        <div className="SideBar-Blue-B">B</div>
+                        <div className="SideBar-Rollo">rollo</div>
+                        </h5>
+                        <button type="button" className="SideBar-Close-Button btn-close" data-bs-dismiss="offcanvas" 
+                        aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <div className="Side-Chest">Home</div>
+                        <div  className="Side-Chest">Boards</div>
+                        <div className="dropend">
+                            <div className="Side-Chest-Toggle dropdown-toggle" 
+                                data-bs-toggle="dropdown" aria-expanded="false" type="button">
+                                Dropdown
+                            </div>
+                            <ul className="dropdown-menu">
+                                <li><button className="dropdown-item" type="button">Engineering</button></li>
+                                <li><button className="dropdown-item" type="button">Marketing</button></li>
+                                <li><button className="dropdown-item" type="button">Sales</button></li>
+                            </ul>
                         </div>
-                        <ul className="dropdown-menu">
-                            <li><button className="dropdown-item" type="button">Engineering</button></li>
-                            <li><button className="dropdown-item" type="button">Marketing</button></li>
-                            <li><button className="dropdown-item" type="button">Sales</button></li>
-                        </ul>
                     </div>
                 </div>
             </div>
