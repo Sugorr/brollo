c0a30c447a2cac8f65c68df05077807b38a2d899
ChrisVerdida <christian.verdida123@gmail.com>
2023-05-12 10:59:32 +0800

    Adding Input Add Card Workspace

--- src/components/BrolloCards/AddCard.js
+++ src/components/BrolloCards/AddCard.js
@@ -1,10 +1,31 @@
 import "./AddCard.css";
+import React, { useState } from 'react';
 function AddCard() {
+    const [editing, setEditing] = useState(false);
+    const [word, setWord] = useState('Example word');
+    const handleInputChange = (event) => {
+        setWord(event.target.value);
+    };
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          setEditing(false);
+        }
+    };
+    const handleEditClick = () => {
+        setEditing(true);
+    };
     return (
         <>
             <div id="Add-Card-Section">
                 <div className="Add-Card-Container">
-                    
+                    <div className="brollo-card">
+                        {editing ? (
+                            <input type="text" value={word} onChange={handleInputChange} 
+                            onKeyDown={handleKeyDown} autoFocus />
+                        ) : (
+                            <p className="Handle-Edit-Word" onClick={handleEditClick}>{word}</p>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
